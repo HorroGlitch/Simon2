302dd6d4b9de59254abd1ba57e764778e79cc914
HorroGlitch <lverot@edenschool.fr>
2023-12-06 09:59:58 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+let redDiv = document.querySelector("#red");
+let greenDiv = document.querySelector("#green");
+let blueDiv = document.querySelector("#blue");
+let yellowDiv = document.querySelector("#yellow");
+
+yellowDiv.addEventListener("click", ()=>{
+  document.
+})
